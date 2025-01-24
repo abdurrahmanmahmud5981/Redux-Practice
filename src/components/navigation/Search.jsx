fe06cb3b5aeb47b702e1ade9ebd26ccf6d7102a5
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-01-24 22:46:35 +0600

    Implement debounced search input and navigate on change

--- src/components/navigation/Search.jsx
+++ src/components/navigation/Search.jsx
@@ -1,29 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setSearchTerm } from "../../features/filter/filterSlice";
 import { useNavigate } from "react-router-dom";
 
-export default function Search(){
-    const navigate = useNavigate()
-    const [input, setInput] = useState('');
-    const dispatch = useDispatch();
-    const handleSubmit = (e) => {
+export default function Search() {
+  const navigate = useNavigate();
+  const [searchDebounced, setSearchDebounced] = useState("");
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  
+  // Debounce search input
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearchDebounced(input.trim());
+      navigate("/");
+      dispatch(setSearchTerm(searchDebounced));;
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [input,searchDebounced,dispatch,navigate]);
+ 
 
-        e.preventDefault();
-        navigate('/')
-        dispatch(setSearchTerm(input))
-    }
-    
-    return (
-        <form onSubmit={handleSubmit} >
-            <input
-                className="outline-none border-none mr-2"
-                type="search"
-                name="search"
-                placeholder="Search"
-                value={input}
-                onChange={(e)=>setInput(e.target.value)}
-            />
-        </form>
-    )
+  return (
+  
+      <input
+        className="outline-none border-none mr-2"
+        type="search"
+        name="search"
+        placeholder="Search"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+  
+  );
 }
